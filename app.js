bffa292af54879d67a1a99a79d9b9df4c763ae21
Naimah Choudhury <naimah.choudhury98@gmail.com>
2026-01-12 13:28:55 +0000

    returns

---
+++ app.js
@@ -0,0 +1,25 @@
+console.log("hello everyone")
+
+function sayHello (){
+    console.log("hello world!")
+}
+
+sayHello ()
+sayHello ()
+
+function sayHelloTo (userName){
+    console.log("hello "+ userName + 
+        "!"
+    )
+}
+
+ sayHelloTo("bob")
+ sayHelloTo("naimah")
+
+function greet(greeting, userName){
+    console.log(greeting + " " + userName)
+
+}
+
+greet("Good Morning", "bob")
+greet("salaam","Naimah")
